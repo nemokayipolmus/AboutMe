7bb9297c8d70e5b11faa9f088c5ae83638ffae1b
Alper Dönmez <44973133+nemokayipolmus@users.noreply.github.com>
2022-04-14 22:21:23 +0300

    Add files via upload

---
+++ Activity6/inclassactivity6.js
@@ -0,0 +1,36 @@
+var timeout = 500;
+var level = 1;
+var count = 0;
+
+var $ = function (id) { return document.getElementById(id); };
+
+window.onload = function () {
+	$("myButton").onmouseover = myHover;
+	$("start").onclick = myStart;
+	$("myButton").onclick = myCount;
+};
+
+function myStart() {
+	document.getElementById("start").style.visibility = "hidden";
+	document.getElementById("myButton").style.visibility = "visible";
+}
+
+function myHover() {
+	setTimeout(changePos, timeout);
+}
+
+function changePos() {
+	document.getElementById("myButton").style.marginLeft = Math.random()*700+"px";
+	document.getElementById("myButton").style.marginTop = Math.random()*400+"px";
+}
+
+function myCount() {
+	count++;
+
+	if (count == 3) {
+		alert("You are the winner!\nGo Next Level: " + (++level));
+		count = 0;
+		timeout = timeout - 100;
+	}
+
+}
