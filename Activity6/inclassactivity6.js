869cf17a2b2a41ab96e0a3f8b7205d8d7e89607e
Alper Dönmez <44973133+nemokayipolmus@users.noreply.github.com>
2022-04-14 22:38:38 +0300

    Update inclassactivity6.js

--- Activity6/inclassactivity6.js
+++ Activity6/inclassactivity6.js
@@ -21,7 +21,7 @@ function myHover() {
 
 function changePos() {
 	document.getElementById("myButton").style.marginLeft = Math.random()*1000+"px";
-	document.getElementById("myButton").style.marginTop = Math.random()*600+"px";
+	document.getElementById("myButton").style.marginTop = Math.random()*500+"px";
 }
 
 function myCount() {
