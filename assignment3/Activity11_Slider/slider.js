eac97c87be83123b09aeab2b879d9e922a604e4d
Alper Dönmez <44973133+nemokayipolmus@users.noreply.github.com>
2022-05-29 23:26:01 +0300

    Add files via upload

---
+++ assignment3/Activity11_Slider/slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	$("#slider").bxSlider({
+		auto: true,
+		minSlides: 1,
+		maxSlides: 1,
+		slideWidth: 250,
+		slideMargin: 10,
+		randomStart: true,
+		autoDelay: 3000,
+		captions: true,
+		pagerType: "short"
+
+	});
+});
+		
